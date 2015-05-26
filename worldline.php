48944c02b2938587a3eacf15e22a17d53bb1e764
rahulbile <rahul.rmcet@gmail.com>
2015-05-26 07:46:28 +0530

    Some more fixes

--- worldline.php
+++ worldline.php
@@ -88,7 +88,7 @@ class osseed_payment_worldline extends CRM_Core_Payment {
   function doTransferCheckout( &$params, $component ) {
     // Build our query string;
     $query_string = '';
-    $atos_params = array(
+    $atos_data_params = array(
       'merchantId' => $this->_paymentProcessor['url_site'],
       'keyVersion' => 1,
       'normalReturnUrl' => '',
@@ -97,39 +97,60 @@ class osseed_payment_worldline extends CRM_Core_Payment {
       'customerIpAddress' => ip_address(),
       'orderId' => $params['invoiceID'],
       'transactionOrigin' => 'CIVICRMPAYMENT',
+      'captureDay' => 0,
+      'captureMode' => 'AUTHOR_CAPTURE',
+      'transactionReference' => $params['invoiceID'],
+      'amount' => $params['amount'],
+      'currencyCode' => $params['currencyID'],
+    );
+    $attached_data = array();
+    // Converts the array into a string of key=value.
+    foreach ($atos_data_params as $key => $value) {
+      $attached_data[] = "$key=$value";
+    }
+    $atos_params_string = implode('|', $attached_data);
+
+    $atosParams = array(
+      'Data' => base64_encode($atos_params_string),
+      'Seal' => worldline_atos_generate_data_seal($atos_params, $this->_paymentProcessor['signature']),
+      'Encode' => 'base64',
+      'InterfaceVersion' => 'HP_2.3',
     );
 
-
-    foreach ($atos_params as $name => $value) {
-      $query_string .= $name . '=' . $value . '&';
-    }
- 
-    // Remove extra &
-    $query_string = rtrim($query_string, '&');
+    require_once 'HTTP/Request.php';
+    $post_params = array(
+      'method' => HTTP_REQUEST_METHOD_POST,
+      'allowRedirects' => TRUE,
+    );
 
     // Atos payment url
     $payment_site_url = $this->_paymentProcessor['url_site'];
-
-    // Redirect the user to the payment url.
-    CRM_Utils_System::redirect($payment_site_url . '?' . $query_string);
- 
-    exit();
+    $request = new HTTP_Request($payment_site_url, $post_params);
+    foreach ($atosParams as $key => $value) {
+      $request->addPostData($key, $value);
+    }
+    $result = $request->sendRequest();
+    if (PEAR::isError($result)) {
+      CRM_Core_Error::fatal($result->getMessage());
+    }
+    //CRM_Utils_System::redirect($request->getUrl());
+    CRM_Utils_System::civiExit();
   }
+}
 
-  /**
-   * Returns a hashed value of data.
-   *
-   * This is used to generate a seal for all requests to ATOS payment servers.
-   *
-   * @param string $data
-   *   Data to convert.
-   * @param array $secret_key
-   *   The secret key of atos wordline account.
-   *
-   * @return string
-   *   return the hashed value.
-   */
-  function worldline_atos_generate_data_seal($data, $secret_key) {
-    return hash('sha256', $data . $secret_key);
-  }
+/**
+ * Returns a hashed value of data.
+ *
+ * This is used to generate a seal for all requests to ATOS payment servers.
+ *
+ * @param string $data
+ *   Data to convert.
+ * @param array $secret_key
+ *   The secret key of atos wordline account.
+ *
+ * @return string
+ *   return the hashed value.
+ */
+function worldline_atos_generate_data_seal($data, $secret_key) {
+  return hash('sha256', $data . $secret_key);
 }
