c4f6150e34aef30857866e200b0c6f771d6a69df
vinay-osseed <vinay@osseed.com>
2023-06-21 17:32:37 +0530

    Added worldline PHP SDK.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'root/com.osseed.payment.worldline',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'fee9f82e2bed7576041f5bd497569484a588f120',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'root/com.osseed.payment.worldline' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'fee9f82e2bed7576041f5bd497569484a588f120',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wl-online-payments-direct/sdk-php' => array(
+            'pretty_version' => '5.5.0',
+            'version' => '5.5.0.0',
+            'reference' => 'b0982676eb30267f4150546a7cafe16f99f5475e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wl-online-payments-direct/sdk-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
